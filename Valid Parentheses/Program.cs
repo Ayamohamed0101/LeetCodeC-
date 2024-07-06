62a61a20da9fef4bbbc3be8614a85aeea9b440e7
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-07-07 00:34:54 +0300

    Add files via upload

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine((int.TryParse("{",out int reslut)));
+            Console.WriteLine(IsValid("()[]{}"));
+
+        }
+        public static bool IsValid(string s)
+        {
+            while (s.Contains("()") || s.Contains("{}")||s.Contains("[]"))
+            {
+                s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            }
+            // '(', ')', '{', '}', '[' , ']'
+            return s.Length == 0;
+
+        }
+    }
+}
