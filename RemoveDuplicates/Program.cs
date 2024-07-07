30d1da7d5047430a3169a8b5a812279c0bb121cd
Aya Mohamed <152428721+Ayamohamed0101@users.noreply.github.com>
2024-07-07 23:05:18 +0300

    Add files via upload

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine(RemoveElement(   array  ,4 ));
+        }
+        public static  int RemoveElement(int[] nums, int val)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[count++] = nums[i];
+
+                }
+
+
+            }
+
+            return count;
+        }
+    }
+}
